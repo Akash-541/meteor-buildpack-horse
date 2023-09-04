2038682065fde51e1065cd187ffb25378d6c52a9
Gonzalo Dutra <gonzalodutrasilva@gmail.com>
2023-09-04 14:02:23 -0300

    first commit

---
+++ extra/prisma.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# echo "-----> Prisma generate"
+# npm install prisma --save-dev
+# cd ~/.meteor/heroku_build/app/programs/server
+# npx prisma generate --schema=./assets/app/prisma/schema.prisma
+# cd node_modules
+# ls
+# cd .prisma
+# ls
